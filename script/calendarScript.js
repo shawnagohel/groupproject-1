30d35f314557c809afb662601b4da7449afbf3e8
RonanSilasSmith <smith.ronan@outlook.com>
2020-09-03 11:43:06 -0800

    adds basic calendar config

---
+++ script/calendarScript.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var calendarEl = document.getElementById('calendar');
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+      initialView: 'dayGridMonth'
+    });
+    calendar.render();
+});
