a1a752160e8225e02795a6a71a21d5b1be1e1d25
RonanSilasSmith <smith.ronan@outlook.com>
2020-09-09 16:46:56 -0800

    allows you to save objects to local storage

--- script/calendarScript.js
+++ script/calendarScript.js
@@ -1,3 +1,19 @@
+function saveMyData(event) {
+  jQuery.post(
+      '/event/save', 
+      {
+          title: event.title,
+          start: event.start,
+          end: event.end,
+          url: event.url
+      }
+  );
+}
+//var a = [{ "title": "new appointment", "start": "2020-09-09"  }];
+//var a = JSON.stringify(a);
+//localStorage.setItem("event", a);
+
+
 document.addEventListener('DOMContentLoaded', function() {
   var calendarEl = document.getElementById('calendar');
 
@@ -9,14 +25,26 @@ document.addEventListener('DOMContentLoaded', function() {
       right: 'addEventButton'//we can change position between 'left' 'right' and 'center'
     },
 
+    events: JSON.parse(localStorage.getItem("event")),
+
     customButtons: {//this lets us create the button for add event
       addEventButton: {
         text: 'Add Meal',//this should pull data from meal planner on click
         click: function() {
 
-          var date = new Date('2020-09-' + prompt('Enter a day DD form')); 
+          var date = new Date(prompt('Enter a date YYYY-MM-DD form') + " 16:00"); 
           var title = "test";
           var url = "https://google.com";
+
+          var saveEvent = [{
+            title: title,
+            start: date,
+            end: date,
+            allDay: true,
+            url: url
+          }];
+
+          localStorage.setItem("event", JSON.stringify(saveEvent));
 
           //ideally date, url and time are selected after selecting a meal
 
@@ -25,6 +53,7 @@ document.addEventListener('DOMContentLoaded', function() {
           calendar.addEvent({
             title: title,
             start: date,
+            end: date,
             allDay: true,//all day lets us not worry about specific times of day
             url: url
           });
