8fe6ccd83d1965fec0434c33ce82754cafa0dd1c
vconte41982 <68178433+vconte41982@users.noreply.github.com>
2020-09-12 12:40:49 -0700

    update html styling

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # groupproject-1
+
--- index.js
+++
@@ -1,9 +0,0 @@
-const calendar = document.querySelector("#app-calendar");
-
-for (let day = 1; day ≤ 31; day++) {
-    console.log(day)
-
-    calendar.insertAdjacentHTML("beforeend" `<div
-    class="day">${day}</div>`);
-}
-     
